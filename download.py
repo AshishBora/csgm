0d45efea342d0e6fab267168e88b51508074d286
AshishBora <ashish.dilip.bora@gmail.com>
2017-03-14 23:37:32 -0500

    Removing mnist unzipping

--- download.py
+++ download.py
@@ -126,9 +126,9 @@ def download_mnist(dirpath):
     cmd = ['curl', url, '-o', out_path]
     print('Downloading ', file_name)
     subprocess.call(cmd)
-    cmd = ['gzip', '-d', out_path]
-    print('Decompressing ', file_name)
-    subprocess.call(cmd)
+    # cmd = ['gzip', '-d', out_path]
+    # print('Decompressing ', file_name)
+    # subprocess.call(cmd)
 
 def prepare_data_dir(path = './data'):
   if not os.path.exists(path):
