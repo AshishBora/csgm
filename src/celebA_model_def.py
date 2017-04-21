63d527e634b5596205ade35029efa36be9ef136c
AshishBora <ashish.dilip.bora@gmail.com>
2017-04-21 06:25:30 -0500

    correction

--- src/celebA_model_def.py
+++ src/celebA_model_def.py
@@ -142,8 +142,7 @@ def dcgan_gen(z, sess, hparams):
                     'g_h3/w',
                     'g_h4/biases',
                     'g_h4/w']
-
     restore_dict = {var.op.name: var for var in tf.global_variables() if var.op.name in restore_vars}
-    restore_path = tf.train.latest_checkpoint('./models/celebA_64_64/')
+    restore_path = tf.train.latest_checkpoint(hparams.pretrained_model_dir)
 
     return x_hat_batch, restore_dict, restore_path
